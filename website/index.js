0ccd96213955484d3b91c6b14d5dbd5d98eb032b
Dinkar Jallan <dinkarjallan@gmail.com>
2021-07-18 12:06:39 +0530

    v-0.1.7
    - added navbar.
    - added responsiveness to navbar.
    - addressed the content correction.
    - addressed minimal performance issues.
    - removed unused styles.
    - navigation of the links REMAINS!

--- website/index.js
+++ website/index.js
@@ -1,10 +1,26 @@
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-    let currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-10%";
-    }
-    prevScrollpos = currentScrollPos;
-}; /* (Scroller animation)When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+// nav slider 
+const navSlide = () => {
+    const burger = document.querySelector('nav.navbar ul.nav-button');
+    const nav = document.querySelector('nav.navbar ul#drawer');
+    const navLinks = document.querySelectorAll('nav.navbar ul.buttons li');
+    const lines = document.querySelectorAll('nav.navbar ul.nav-button div');
+
+    burger.addEventListener('click', () => {
+        // toggle nav
+        nav.classList.toggle('nav-active');
+
+        // animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+
+            }
+        });
+        // burger animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
